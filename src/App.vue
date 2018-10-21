8eeedb774ff491dfaa23a18c8642872662bfbc1e
Oxygent02 <33537279+Oxygent02@users.noreply.github.com>
2018-10-21 15:44:28 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <navbar-starwars></navbar-starwars>
+    <float-button  @character-added="showCharacters"></float-button>
+    <character-list :characters="characters"></character-list>
+  </div>
+</template>
+
+<script>
+import navbarStarwars from "./components/navbarStarwars";
+import floatButton from "./components/floatButton";
+import characterList from "./components/characterList";
+
+export default {
+  name: "App",
+  components: {
+    navbarStarwars,
+    floatButton,
+    characterList
+  },
+  data(){
+    return{
+      characters: []
+    }
+  },
+  methods:{
+    showCharacters(payload){
+      this.characters = payload;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  position: relative;
+}
+</style>
---
+++ src/components/navbarStarwars.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav>
+      <div class="nav-wrapper">
+        <form>
+          <div class="input-field">
+            <input id="search" type="search" required>
+            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+            <i class="material-icons">close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+    <p>{{username}}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      username: ""
+    };
+  },
+  mounted() {
+    this.username = "Daftar Nama:";
+  }
+};
+</script>
